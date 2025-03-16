af6d0cb21d514d1d916bb4aa5509c4c237ae58c3
Onlyoneariom <ariom6696@gmail.com>
2025-03-16 18:00:16 +0100

    Add files via upload

--- a.py
+++ a.py
@@ -1,3 +1,15 @@
-n = 22
-for i in range (1, 11):
-  print (n, 'X', i, '=',n*i)
+class person(object):
+    def __init__(self, name, idnumber):
+           self.name = name 
+           self.idnumber = idnumber
+    def display(self):
+          print(self.name)
+          print(self.idnumber)
+class Employeer(person):
+    def __init__(self, name, idnumber, salary, post):
+         
+         self.salary = salary
+         self.post = post
+         person.__init__(self, name, idnumber)
+a=Employeer('Angel', 202222, 122222, "Intern")      
+a.display()           
